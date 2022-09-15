d6e1fb2488d5763ec4a9ff2198a3b1e571c7d6a3
polyloly <112742178+polyloly@users.noreply.github.com>
2022-09-15 07:42:17 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -9,14 +9,14 @@ cat << EOF > /etc/v2ray/config.json
         "protocol": "vmess",
         "settings": {
             "clients": [{
-                "id": "2112b632-fb6c-4781-bb42-d4d310aa532f",
+                "id": "4d599805-a74d-48c0-9404-72fa42da027a",
                 "alterId": 0
             }]
         },
         "streamSettings": {
             "network": "ws",
             "wsSettings": {
-                "path": "/laowang"
+                "path": "/kkl"
             }
         }
     }],
